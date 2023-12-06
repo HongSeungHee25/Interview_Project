0a6a437d0c051876bd6d7cef1f8029d7d798cae9
홍승희 <seung0476@gmail.com>
2023-12-06 14:08:47 +0900

    init

---
+++ src/test/java/org/interview/mvc/InterviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.interview.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
